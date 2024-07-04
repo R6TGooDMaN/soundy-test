4068a7415668c5c4e7a221eb7206beb8c6e8973e
Mataffi <chvarkov.nikita@gmail.com>
2024-07-04 13:50:26 +0300

    Release

--- Soundy/src/main/java/mataffi/soundy/SoundyApplication.java
+++ Soundy/src/main/java/mataffi/soundy/SoundyApplication.java
@@ -11,9 +11,7 @@ import static mataffi.soundy.utilities.Serialization.deserializeHashMap;
 
 @SpringBootApplication
 public class SoundyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SoundyApplication.class, args);
     }
-
 }
